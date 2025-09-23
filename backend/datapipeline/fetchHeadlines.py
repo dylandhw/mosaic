1e1f01211245730bd5f106907face32a40b795b3
dylandhw <dylanwilkins2308@gmail.com>
2025-09-23 03:19:58 -0400

    some stuff

---
+++ backend/datapipeline/fetchHeadlines.py
@@ -0,0 +1,34 @@
+import feedparser 
+import json
+
+headlines = []
+
+rssFeeds = [
+    "https://feeds.npr.org/1001/rss.xml",
+    "http://rss.cnn.com/rss/cnn_topstories.rss",
+    "http://rssfeeds.usatoday.com/usatoday-NewsTopStories",
+    "http://newsrss.bbc.co.uk/rss/newsonline_world_edition/americas/rss.xml"
+    "https://feeds.nbcnews.com/nbcnews/public/news",
+    "https://abcnews.go.com/abcnews/topstories",
+    "https://www.cbsnews.com/latest/rss/main",
+    "https://www.politico.com/rss/politicopicks.xml",
+    "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+    "https://www.washingtontimes.com/rss/headlines/news",
+    "https://www.latimes.com/local/rss2.0.xml"
+]
+count=0
+for url in rssFeeds:
+    feed = feedparser.parse(url)
+    for entry in feed.entries:
+        count += 1
+        headlines.append({
+            "source": feed.feed.title,
+            "title": entry.title,
+            "link": entry.link,
+            "published": entry.get("published", None)
+        })
+print(count)
+
+with open("headlines.json", "w", encoding="utf-8") as f:
+    json.dump(headlines, f, indent=4, ensure_ascii=False)
+print(">> Complete <<")
